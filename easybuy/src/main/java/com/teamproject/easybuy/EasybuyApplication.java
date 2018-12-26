0270cbb5ad94fdf97cb0195f2c52f269da59b720
the9candme <305325266@qq.com>
2018-12-26 15:45:52 +0800

    新建模块

---
+++ easybuy/src/main/java/com/teamproject/easybuy/EasybuyApplication.java
@@ -0,0 +1,14 @@
+package com.teamproject.easybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybuyApplication.class, args);
+	}
+
+}
+
